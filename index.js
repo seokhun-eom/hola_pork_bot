8901061309826888cd68ba4596f7cc3159963a63
seokhun-eom <seokhun_eom@tmax.co.kr>
2024-05-31 10:26:25 +0900

    코드 리팩토링
    test

--- index.js
+++ index.js
@@ -2,105 +2,116 @@ import puppeteer from "puppeteer";
 import "dotenv/config";
 
 const isDevMode = process.env.NODE_ENV === "development";
-const browserConfig = isDevMode
-  ? {
-      headless: false,
-    }
-  : {};
+const browserConfig = {
+  headless: !isDevMode,
+};
+
+async function getRecentPostLink(page) {
+  await page.goto("https://www.instagram.com/hola_pork", {
+    waitUntil: "networkidle2",
+  });
+  await page.waitForSelector("._aagu");
+  return await page.evaluate(
+    () => document.querySelector("._aagu").parentElement.href
+  );
+}
+
+async function extractTextFromPost(page) {
+  await page.waitForSelector("._a9zs");
+  return await page.evaluate(() => {
+    const elements = Array.from(document.querySelectorAll("*"));
+    const targetElement = elements.find((el) =>
+      el.textContent.includes("올라밥집")
+    );
+    return targetElement ? targetElement.innerText : "";
+  });
+}
 
 async function getText() {
   const browser = await puppeteer.launch(browserConfig);
   const page = await browser.newPage();
+  await page.setViewport({ width: 1280, height: 1080 });
 
-  await page.goto("https://www.instagram.com/", { waitUntil: "networkidle2" });
-  await page.type('input[name="username"]', process.env.INSTAGRAM_USERNAME);
-  await page.type('input[name="password"]', process.env.INSTAGRAM_PASSWORD);
-  await page.click('button[type="submit"]');
-  await page.waitForNavigation();
-
-  await page.goto(`https://www.instagram.com/hola_pork`, {
-    waitUntil: "networkidle2",
-  });
-
-  const recentPost = await page.evaluate(
-    () => document.querySelector("._aagu").parentElement.href
-  );
-
-  await page.goto(recentPost, {
-    waitUntil: "networkidle2",
-  });
-
-  const text = await page.evaluate(() => {
-    const elements = Array.from(document.querySelectorAll("span"));
-    const hola = elements.filter((el) => el.textContent.includes("올라밥집"));
-    if (!hola[0]) return "";
-    return hola[0].innerText;
-  });
-
-  await browser.close();
-
-  return text;
+  try {
+    const recentPostLink = await getRecentPostLink(page);
+    await page.goto(recentPostLink, { waitUntil: "networkidle2" });
+    const text = await extractTextFromPost(page);
+    return text;
+  } finally {
+    await browser.close();
+  }
 }
 
-async function writeText(str) {
-  const browser = await puppeteer.launch(browserConfig);
-  const page = await browser.newPage();
-  await page.goto("https://wapl.ai/spaces", {
-    waitUntil: "networkidle2",
-  });
-
+async function loginToWapl(page) {
+  await page.goto("https://wapl.ai/spaces", { waitUntil: "networkidle2" });
+  await page.waitForSelector('input[name="username"]');
   await page.type('input[name="username"]', process.env.WAPL_USERNAME);
   await page.type('input[name="password"]', process.env.WAPL_PASSWORD);
   await page.click('input[name="login"]');
   await page.waitForNavigation();
+}
 
-  await page.goto(
-    isDevMode ? process.env.WAPL_ROOM_DEV : process.env.WAPL_ROOM_PROD,
-    {
-      waitUntil: "networkidle2",
-    }
-  );
+async function postMenuToWapl(page, menu) {
+  const roomUrl = isDevMode
+    ? process.env.WAPL_ROOM_DEV
+    : process.env.WAPL_ROOM_PROD;
+  await page.goto(roomUrl, { waitUntil: "networkidle2" });
   await page.waitForSelector('div[data-placeholder="Enter a message."]');
-
   await page.evaluate((menu) => {
     const input = document.querySelector(
       'div[data-placeholder="Enter a message."]'
     );
     const month = new Date().getMonth() + 1;
     const date = new Date().getDate();
-
     input.innerHTML = `<div>🤖올라술고기 메뉴 봇 - ${month}월 ${date}일 점심 메뉴</div><div>${menu}</div>`;
-  }, str);
+  }, menu);
+  await page.waitForSelector('button[class="sc-kQwWFH dGAtdu"]');
   await page.click('button[class="sc-kQwWFH dGAtdu"]');
-
-  await browser.close();
+  await (() => new Promise((res) => setTimeout(res, 1000)))();
 }
 
-function extractMenu(str) {
-  const lines = str.split("\n");
+async function writeText(menu) {
+  const browser = await puppeteer.launch(browserConfig);
+  const page = await browser.newPage();
+  await page.setViewport({ width: 1280, height: 1080 });
 
-  const menu = lines
+  try {
+    await loginToWapl(page);
+    await postMenuToWapl(page, menu);
+  } finally {
+    await browser.close();
+  }
+}
+
+function extractMenu(text) {
+  return text
+    .split("\n")
     .filter((line) => line.startsWith("✔"))
     .map((line) => line.replace("✔", "").trim());
-
-  return menu;
 }
 
-function checkToday(str) {
+function checkToday(text) {
   const month = new Date().getMonth() + 1;
   const date = new Date().getDate();
-
   return (
-    str.includes(`${month}월 ${date}일`) || str.includes(`${month}월${date}일`)
+    text.includes(`${month}월 ${date}일`) ||
+    text.includes(`${month}월${date}일`)
   );
 }
 
 async function main() {
-  const text = await getText();
-  const isToday = checkToday(text);
-  if (!isToday) return;
-  const menu = extractMenu(text);
-  await writeText(menu.join(", "));
+  try {
+    const text = await getText();
+    if (!isDevMode && !checkToday(text)) {
+      console.log("It's not today's menu");
+      return;
+    }
+    const menu = extractMenu(text);
+    console.log(menu);
+    await writeText(menu.join(", "));
+  } catch (error) {
+    console.error("Error occurred:", error);
+  }
 }
 
 await main();
